5d4efffbc50d995e2b83572ac06e6b723c0a63d0
Gen1001 <genshiro13@gmail.com>
2024-05-27 23:15:35 +0900

    kadai_tutorial_outputを修正

--- kadai_tutorial_output/main.js
+++ kadai_tutorial_output/main.js
@@ -9,23 +9,20 @@ const start = document.getElementById("start");
 const count = document.getElementById("count");
 
 const textLists = [
-    -  "Hello World",
-    -  "This is my App",
-    -  "How are you?"
-    +  "Hello World","This is my App","How are you?",
-    +  "Today is sunny","I love JavaScript!","Good morning",
-    +  "I am Japanese","Let it be","Samurai",
-    +  "Typing Game","Information Technology",
-    +  "I want to be a programmer","What day is today?",
-    +  "I want to build a web app","Nice to meet you",
-    +  "Chrome Firefox Edge Safari","machine learning",
-    +  "Brendan Eich","John Resig","React Vue Angular",
-    +  "Netscape Communications",
-    +  "Thank you very much","Google Apple Facebook Amazon",
-    +  "ECMAScript","console.log","for while if switch",
-    +  "var let const","Windows Mac Linux iOS Android",
-    +  "programming"
-];    
+    'Hello World','This is my App','How are you?',
+    'Today is sunny','I love JavaScript!','Good morning',
+    'I am Japanese','Let it be','Samurai',
+    'Typing Game','Information Technology',
+    'I want to be a programmer','What day is today?',
+    'I want to build a web app','Nice to meet you',
+    'Chrome Firefox Edge Safari','machine learning',
+    'Brendan Eich','John Resig','React Vue Angular',
+    'Netscape Communications','undefined null NaN',
+    'Thank you very much','Google Apple Facebook Amazon',
+    'ECMAScript','console.log','for while if switch',
+    'var let const','Windows Mac Linux iOS Android',
+    'programming'
+  ];
 
 const createText = () => {
 
@@ -48,6 +45,7 @@ const keyPress = e => {
     }
 
     score++;
+    wrap.classList.remove("mistyped");
     typed += untyped.substring(0, 1);
     untyped = untyped.substring(1);
     typedfield.textContent = typed;
@@ -77,7 +75,7 @@ const rankCheck = score => {
 const gameOver = id => {
     clearInterval(id);
 
-    const resule = confirm(rankCheck(score));
+    const result = confirm(rankCheck(score));
 
     if(result == true) {
         window.location.reload();
